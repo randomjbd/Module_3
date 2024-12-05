2bbe1bdb892f855f417e0fba39fb42904b3f6bf6
ПКНЗ <@DESKTOP-OGI312N>
2024-12-05 13:44:16 +0300

    Добавьте файлы проекта.

---
+++ Module_3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+
+        {
+            string Name = "PKNZ";
+            Console.WriteLine("Меня зовут {0} ",Name);
+            Console.WriteLine("мне 38 лет");
+            Console.WriteLine("у меня есть питомец? " + true);
+            Console.WriteLine("мой размер ноги " + 41);
+            Console.ReadKey();
+
+        }
+    }
+}
