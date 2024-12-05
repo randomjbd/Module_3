d9a673fa7e122387553c19a6ed37a5ea863ef6e3
ПКНЗ <@DESKTOP-OGI312N>
2024-12-05 13:45:15 +0300

    Перенес последнюю строку

--- Module_3/Program.cs
+++ Module_3/Program.cs
@@ -10,7 +10,7 @@ class MainClass
             Console.WriteLine("Меня зовут {0} ",Name);
             Console.WriteLine("мне 38 лет");
             Console.WriteLine("у меня есть питомец? " + true);
-            Console.WriteLine("мой размер ноги " + 41);
+            Console.WriteLine("\nмой размер ноги " + 41);
             Console.ReadKey();
 
         }
