5dfb978bba16b3bed7b9a1e2a692c7d41da5bf9a
ПКНЗ <@DESKTOP-OGI312N>
2024-12-05 13:51:21 +0300

    поправил как в примере

--- Module_3/Program.cs
+++ Module_3/Program.cs
@@ -7,10 +7,13 @@ class MainClass
 
         {
             string Name = "PKNZ";
+            Byte age = 38;
+            bool pet = true;
+            float noga = 41.5f;
             Console.WriteLine("Меня зовут {0} ",Name);
-            Console.WriteLine("мне 38 лет");
-            Console.WriteLine("у меня есть питомец? " + true);
-            Console.WriteLine("\nмой размер ноги " + 41);
+            Console.WriteLine("мне {0} лет",age);
+            Console.WriteLine("у меня есть питомец? " + pet);
+            Console.WriteLine("\nмой размер ноги " + noga);
             Console.ReadKey();
 
         }
